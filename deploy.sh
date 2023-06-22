9f5415f701a744f9cd18b07641037a1feb8a28b7
Pedro Fouto <pfouto10@gmail.com>
2023-06-22 16:46:33 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 mvn clean package
 
 echo "Copying to cluster"
-rsync -azvhuP -r --delete --exclude 'venv' --exclude=".*" deploy/client/* cluster:/home/pfouto/edge/client
+rsync -azvhuP -r --delete --exclude 'venv' --exclude=".*" deploy/* cluster:/home/pfouto/edge/client
 echo "Copying to grid"
-rsync -azvhuP -r --delete --exclude 'venv' --exclude=".*" deploy/client/* nancy.g5k:/home/pfouto/edge/client
+rsync -azvhuP -r --delete --exclude 'venv' --exclude=".*" deploy/* nancy.g5k:/home/pfouto/edge/client
 echo "Done"
